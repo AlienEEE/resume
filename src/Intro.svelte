9e41755fdbdbc202b6eabfe63e519d43d4263af2
thanyaratp <624234009@parichat.skru.ac.th>
2021-07-15 12:20:14 +0700

    :memo: update color

--- src/Intro.svelte
+++ src/Intro.svelte
@@ -4,7 +4,7 @@
   export let phone: string = ""
   export let email: string = ""
   export let github: string = ""
-  export let linkedin: string = ""
+  // export let linkedin: string = ""
   export let location: string = ""
   export let website: string = ""
 </script>
@@ -38,17 +38,17 @@
         >{website}</a
       >
     </p>
-    <p>
+    <!-- <p>
       <a
         href={`https://linkedin.com/in/${linkedin}`}
         target="_blank"
         rel="noreferrer">Linkedin</a
       >
-    </p>
+    </p> -->
   </div>
 </div>
 
-<style>
+<style> 
   a {
     text-decoration: underline;
   }
